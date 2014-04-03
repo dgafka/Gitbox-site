5ccfb67da5464b9dd5dc790b1114eb6d401c07ed
gruchanet <grucha.net@gmail.com>
2014-04-03 14:00:14 +0200

    fix register routing

--- src/Gitbox/ApplicationBundle/Controller/RegisterController.php
+++ src/Gitbox/ApplicationBundle/Controller/RegisterController.php
@@ -1,15 +1,17 @@
-namespace Gitbox\ApplicationBundle\Register;
+<?php
 
-    use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-    // Import new namespaces
-    use Gitbox\ApplicationBundle\Entity\UserAccount;
-    use Gitbox\ApplicationBundle\\Form\EnquiryType;
+namespace Gitbox\ApplicationBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// Import new namespaces
+use Gitbox\ApplicationBundle\Entity\UserAccount;
+use Gitbox\ApplicationBundle\Form\EnquiryType;
 
 class RegisterController extends Controller
 {
     public function registerAction()
     {
-        $enquiry = new Enquiry();
+        $enquiry = new UserAccount();
         $form = $this->createForm(new EnquiryType(), $enquiry);
 
         $request = $this->getRequest();
@@ -21,7 +23,7 @@ class RegisterController extends Controller
 
                 // Redirect - This is important to prevent users re-posting
                 // the form if they refresh the page
-                return $this->redirect($this->generateUrl('GitboxApplicationBundle_register'));
+                return $this->redirect($this->generateUrl('gitbox_application_register'));
             }
         }
 
@@ -29,6 +31,4 @@ class RegisterController extends Controller
             'form' => $form->createView()
         ));
     }
-
-
 }
