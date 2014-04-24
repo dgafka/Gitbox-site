235d94ccb623d25c7a083ad81106fa10a127717e
gruchanet <grucha.net@gmail.com>
2014-04-24 02:29:48 +0200

    fix

--- src/Gitbox/Bundle/CoreBundle/Controller/BlogController.php
+++ src/Gitbox/Bundle/CoreBundle/Controller/BlogController.php
@@ -76,6 +76,14 @@ class BlogController extends Controller
     }
 
     /**
+     * @Route("/user/{login}/blog/{id}/edit")
+     * @Template()
+     */
+    public function editAction($login, $id)
+    {
+    }
+
+    /**
      * @Route("/user/{login}/blog/{id}", name="user_show_post")
      * @Template()
      */
@@ -92,14 +100,6 @@ class BlogController extends Controller
         );
 
         return array('user' => $user, 'post' => $post);
-    }
-
-    /**
-     * @Route("/user/{login}/blog/{id}/edit")
-     * @Template()
-     */
-    public function editAction($login, $id)
-    {
     }
 
 }
