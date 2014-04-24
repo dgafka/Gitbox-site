d3186cb4138a02c29fb9a98dd7bbbbe41a6810ce
dgafka <dariss6666@gmail.com>
2014-04-24 15:04:02 +0200

    User Profile tempaltes

--- src/Gitbox/Bundle/CoreBundle/Controller/UserProfileController.php
+++ src/Gitbox/Bundle/CoreBundle/Controller/UserProfileController.php
@@ -10,35 +10,45 @@ class UserProfileController extends Controller
 {
 
 	/** Akcja dla ukazania profilu usera
-	 * @Route("user/{login}", name="user_profile_url")
+	 * @Route("user/{login}", name="user_profile_index")
 	 * @Template()
 	 */
 	public function indexAction($login) {
+		return array('user' => $this->getUserByLogin($login));
+	}
 
+    /**
+     * @Route("/user/{login}/modules", name="user_profile_modules")
+     * @Template()
+     */
+    public function modulesAction($login) {
+	    return array('user' => $this->getUserByLogin($login));
+    }
+
+    /**
+     * @Route("/user/{login}/about", name="user_profile_about")
+     * @Template()
+     */
+    public function aboutAction($login) {
+		return array('user' => $this->getUserByLogin($login));
+    }
+
+	/**
+	 * @param $login
+	 * @Route("/user/{login}/search", name="user_profile_search")
+	 * @Template()
+	 */
+	public function searchAction($login) {
+		return array('user' => $this->getUserByLogin($login));
+	}
+
+	private function getUserByLogin($login) {
 		$em = $this->getDoctrine()->getManager();
 		$user = $em->getRepository('\Gitbox\Bundle\CoreBundle\Entity\UserAccount')->findOneBy(array('login' => $login));
 		if(!$user instanceof \Gitbox\Bundle\CoreBundle\Entity\UserAccount) {
 			throw $this->createNotFoundException("Nie znaleziono podanego użytkownika");
 		}
 
-		return array('user' => $user);
+		return $user;
 	}
-
-    /**
-     * @Route("/user/{user}/modules")
-     * @Template()
-     */
-    public function modulesAction($user)
-    {
-
-    }
-
-    /**
-     * @Route("/user/{user}/about")
-     * @Template()
-     */
-    public function aboutAction($user)
-    {
-    }
-
 }
