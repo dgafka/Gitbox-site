275d7f9e64e56d0cf761738b0efe3b5149026471
dgafka <dariss6666@gmail.com>
2014-04-25 20:07:53 +0200

    Abstract Helper for helpers done. + User Account service done.

--- src/Gitbox/Bundle/CoreBundle/Helper/Helper.php
+++ src/Gitbox/Bundle/CoreBundle/Helper/Helper.php
@@ -1,10 +1,42 @@
 <?php
 namespace Gitbox\Bundle\CoreBundle\Helper;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-abstract class Helper extends Controller {
 
-	public
+/** Klasa abstrakcyjna po której powinny dziedzić helpery do wyciągania danych z bazy.
+ * Class Helper
+ * @package Gitbox\Bundle\CoreBundle\Helper
+ */
+abstract class Helper {
+
+	/**
+	 * @var ObjectManager
+	 */
+	protected static $em;
+
+	/** Pobranie entity managera z containera symfony
+	 * @param $entityManager
+	 */
+	public function __construct($entityManager) {
+		self::$em = $entityManager;
+	}
+
+	public function instance() {
+		if(!isset(self::$em)) {
+			self::$em = $this->getDoctrine()->getManager();
+		}
+
+		return self::$em;
+	}
+
+	public abstract function find($object);
+
+	public abstract function remove($object);
+
+	public abstract function update($object);
+
+	public abstract function insert($object);
 
 } 
--- src/Gitbox/Bundle/CoreBundle/Helper/UserAccountHelper.php
+++ src/Gitbox/Bundle/CoreBundle/Helper/UserAccountHelper.php
@@ -1,16 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: daris
- * Date: 4/24/14
- * Time: 3:43 PM
- */
 
 namespace Gitbox\Bundle\CoreBundle\Helper;
+use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
-class UserAccount extends Helper {
+/** Klasa wspomagająca User Account
+ * Class UserAccountHelper
+ * @package Gitbox\Bundle\CoreBundle\Helper
+ */
+class UserAccountHelper extends Helper {
 
+	public function __construct($entityManager) {
+		parent::__construct($entityManager);
+	}
+
+	/** Wyszukuje obiekt po id
+	 * @param $object \Gitbox\Bundle\CoreBundle\Entity\UserAccount|int
+	 * @return null|\Gitbox\Bundle\CoreBundle\Entity\UserAccount
+	 * @throws \Exception
+	 */
 	public function find($object)
 	{
 		$userAccount = null;
@@ -18,41 +27,83 @@ class UserAccount extends Helper {
 			$userAccount = $this->instance()->getRepository('\Gitbox\Bundle\CoreBundle\Entity\UserAccount')->find($object->getId());
 		}else if(is_int((int)$object)) {
 			$userAccount = $this->instance()->getRepository('\Gitbox\Bundle\CoreBundle\Entity\UserAccount')->find($object);
+		}else {
+			throw new Exception("Błąd podczas wyszukiwania obiektu.");
 		}
 
 		return $userAccount;
 
 	}
 
+	/** Znajduje jeden obiekt po loginie
+	 * @param $login
+	 * @return \Gitbox\Bundle\CoreBundle\Entity\UserAccount
+	 */
+	public function findByLogin($login) {
+		$userAccount = $this->instance()->getRepository('\Gitbox\Bundle\CoreBundle\Entity\UserAccount')->findOneBy(array('login' => $login));
+		return $userAccount;
+	}
+
+	/** Usuwa rekord z tabeli User Account po id
+	 * @param $object \Gitbox\Bundle\CoreBundle\Entity\UserAccount|int
+	 * @return \Gitbox\Bundle\CoreBundle\Entity\UserAccount|int
+	 * @throws \Exception
+	 */
 	public function remove($object)
 	{
 		if($object instanceof \Gitbox\Bundle\CoreBundle\Entity\UserAccount) {
 			$userAccount = $this->instance()->remove($object);
-		}else if(is_int((int)$object)) {
-	    /**
-	     * @var $queryBuilder QueryBuilder
-	     */
-	    $queryBuilder = $this->getDoctrine()->getManager()->createQueryBuilder();
-	    $results = $queryBuilder
-		    ->delete('\Gitbox\Bundle\CoreBundle\Entity\UserAccount', 'ua')
-			->where('ua.id = :id')
-		    ->setParameter('ua', $object)
-		    ->getQuery()
-		    ->execute();
+		}else if(is_int($object)) {
+		    /**
+		     * @var $queryBuilder QueryBuilder
+		     */
+		    $queryBuilder = $this->instance()->createQueryBuilder();
+		    $queryBuilder
+			    ->delete('\Gitbox\Bundle\CoreBundle\Entity\UserAccount', 'ua')
+				->where('ua.id = :id')
+			    ->setParameter('id', $object)
+			    ->getQuery()
+			    ->execute();
+		}else {
+			throw new Exception("Błąd podczas usuwania obiektu.");
 		}
 
 		return $object;
 
 	}
 
+	/** Uaktualnia obiekt
+	 * @param $object \Gitbox\Bundle\CoreBundle\Entity\UserAccount|int
+	 * @throws \Exception
+	 * @return \Gitbox\Bundle\CoreBundle\Entity\UserAccount
+	 */
 	public function update($object)
 	{
-		// TODO: Implement update() method.
+		if($object instanceof \Gitbox\Bundle\CoreBundle\Entity\UserAccount) {
+			$userAccount = $this->instance()->persist($object);
+			$this->instance()->flush();
+		}else {
+			throw new Exception("Błąd podczas uaktualniania obiektu.");
+		}
+
+		return $object;
 	}
 
+	/** Wstawia obiekt
+	 * @param $object \Gitbox\Bundle\CoreBundle\Entity\UserAccount|int
+	 * @throws \Exception
+	 * @return \Gitbox\Bundle\CoreBundle\Entity\UserAccount
+	 */
 	public function insert($object)
 	{
-		// TODO: Implement insert() method.
+		if($object instanceof \Gitbox\Bundle\CoreBundle\Entity\UserAccount) {
+			$userAccount = $this->instance()->persist($object);
+			$this->instance()->flush();
+		}else {
+			throw new Exception("Błąd podczas dodawania obiektu.");
+		}
+
+		return $object;
 	}
 
 } 
