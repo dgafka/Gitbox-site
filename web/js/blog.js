8def8ee4fe82757d642f13c124685bb470cb4349
gruchanet <grucha.net@gmail.com>
2014-05-06 01:00:23 +0200

    add post removal modal

--- web/js/blog.js
+++ web/js/blog.js
@@ -1,3 +1,33 @@
 $(document).ready(function () {
+    /* Tooltips-y `Feel soo dipsy~` */
     $('.btn-float-left').tooltip();
+
+    /* Post remove modal - wypełniany treścią po kliknięciu na button `Usuń` */
+    $(function () {
+        var modal = $('#postRemoveModal');
+
+        var modalTitle = $(modal).find('.modal-title');
+        var modalBody = $(modal).find('.modal-body');
+        var confirmBtn = $(modal).find('.modal-footer .btn-remove-confirm');
+
+        $('.btn-post-remove').click(function () {
+            var removalLink = $(this).attr('data-link');
+            var postTitle = $(this).parent('.post-in-the-box').find('.post-title').first().html();
+
+            $(modalTitle).html('Usuń wpis <b>' + postTitle + '</b>');
+            $(modalBody).html(
+                '<p>Czy na pewno chcesz usunąć wpis <b>' + postTitle + '</b></p>' +
+                    '<p>Usunięte zostaną również jego komentarze, twoje oceny oraz ogólna zawartość.</p>'
+            );
+            $(confirmBtn).attr('href', removalLink);
+
+            $('#postRemoveModal').modal('show');
+        });
+
+        $('#postRemoveModal').on('hidden.bs.modal', function () {
+            $(modalTitle).empty();
+            $(modalBody).empty();
+            $(confirmBtn).attr('href', '#');
+        });
+    });
 });
