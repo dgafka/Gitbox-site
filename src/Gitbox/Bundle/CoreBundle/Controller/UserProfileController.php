7780f1005101bc39185fb1e78ae4e7b9a9758cc4
gruchanet <grucha.net@gmail.com>
2014-05-07 21:49:55 +0200

    profile date fix

--- src/Gitbox/Bundle/CoreBundle/Controller/UserProfileController.php
+++ src/Gitbox/Bundle/CoreBundle/Controller/UserProfileController.php
@@ -33,7 +33,7 @@ class UserProfileController extends Controller
 		$permissionHelper = $this->container->get('permissions_helper');
 		$owner            = $permissionHelper->checkPermission($login);
 
-		return array('login' => $user->getLogin(), 'email' => $user->getEmail(), 'userGroup' => $user->getIdGroup()->getDescription(),'description' => $userDescription->getContent(), 'registerDate' => $userDescription->getRegistrationDate()->format('Y-m-d H:m:s'), 'isOwner' => $owner);
+		return array('login' => $user->getLogin(), 'email' => $user->getEmail(), 'userGroup' => $user->getIdGroup()->getDescription(),'description' => $userDescription->getContent(), 'registerDate' => $userDescription->getRegistrationDate()->format('Y-m-d H:i:s'), 'isOwner' => $owner);
 	}
 
     /**
