b1be6938ddd633ef16297c93f6335aff2924ea91
gruchanet <grucha.net@gmail.com>
2014-05-09 23:27:25 +0200

    remove unused usage

--- src/Gitbox/Bundle/CoreBundle/Controller/UserAccountController.php
+++ src/Gitbox/Bundle/CoreBundle/Controller/UserAccountController.php
@@ -16,7 +16,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 
 class UserAccountController extends Controller
